2ac767da81e03f25caddee905b9ac2cf1325be82
Freya-The-Goddess <106995979+Freya-The-Goddess@users.noreply.github.com>
2023-04-29 00:46:08 +1000

    Initial commit

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/cos30043/s102584470/vue-cli-vuetify-test/'
+    : '/',
+  pluginOptions: {
+    vuetify: {
+      // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+    }
+  }
 })
