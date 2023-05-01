8467d24f6b2b60186f68a0b09d1f1d80dc97e865
Freya-The-Goddess <106995979+Freya-The-Goddess@users.noreply.github.com>
2023-05-01 16:33:36 +1000

    Config files

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/cos30043/s102584470/vue-cli-vuetify-test/'
+    ? '/cos30043/s102584470/vic-transport/'
     : '/',
   pluginOptions: {
     vuetify: {
