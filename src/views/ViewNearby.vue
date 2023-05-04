038118a83a432443153db07921a109a2fc338e1b
Freya-The-Goddess <106995979+Freya-The-Goddess@users.noreply.github.com>
2023-05-05 03:20:25 +1000

    Added components to Nearby page

--- src/views/ViewNearby.vue
+++ src/views/ViewNearby.vue
@@ -23,12 +23,49 @@
 <script>
 import { defineComponent } from 'vue'
 
+// Child components
+import RouteTypeSelect from '../components/SectionRouteTypeSelect.vue'
+import StopList from '../components/SectionStopList.vue'
+
 export default defineComponent({
   name: 'ViewNearby',
 
+  components: { // Child components
+    RouteTypeSelect,
+    StopList
+  },
+
   data: function () { // Default data
     return {
-      jsonStops: []
+      locationPermission: false,
+      location: { lat: 0, long: 0 },
+      jsonStops: [],
+      filterRouteTypes: []
+    }
+  },
+
+  mounted: function () {
+    this.getLocation()
+  },
+
+  methods: {
+    getLocation: function () {
+      const self = this
+      navigator.geolocation.getCurrentPosition(
+      function (position) {
+        self.locationPermission = true
+        self.location = { lat: position.coords.latitude, long: position.coords.longitude }
+        console.log(self.location)
+      },
+      function () {
+        self.locationPermission = false
+      })
+    },
+
+    // Get route types callback for child component $emit event
+    getRouteTypes: function (value) {
+      this.filterRouteTypes = value
+      // run search
     }
   }
 })
