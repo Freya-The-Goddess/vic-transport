23483eaec047709e61eee650a2e2db7edda38a1f
Freya-The-Goddess <106995979+Freya-The-Goddess@users.noreply.github.com>
2023-06-01 21:52:32 +1000

    Fixed header on smaller devices

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -4,7 +4,10 @@
       <v-row>
         <v-col class='d-flex align-center pb-0'>
           <app-logo class='d-inline-block me-1'></app-logo>
-          <h1 class='text-h4 font-weight-bold d-inline-block mt-1 ms-1'>Vic Transport</h1>
+          <h1
+            class='text-h4 font-weight-bold d-inline-block mt-1 ms-1'
+            :class='xs ? "title-xs" : ""'
+          >Vic Transport</h1>
         </v-col>
       </v-row>
     </v-container>
@@ -12,14 +15,27 @@
 </template>
 
 <script>
+import { useDisplay } from 'vuetify'
+
 // Child components
 import AppLogo from '../components/AppLogo.vue'
 
 export default {
   name: 'AppHeader',
 
+  setup () {
+    const { xs } = useDisplay()
+    return { xs }
+  },
+
   components: { // Child components
     AppLogo
   }
 }
 </script>
+
+<style scoped>
+.title-xs {
+  font-size: min(2.125rem, 8vw) !important;
+}
+</style>
